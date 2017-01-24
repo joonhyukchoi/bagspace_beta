1254d7682bbda72756b7cdcbdda9027dcfde9a45
YoujunLee <injxyj@gmail.com>
2017-01-24 13:22:53 +0900

    안 쓰는 모듈들 삭제

--- bagspace_application/src/app/modules/profile.module.ts
+++ bagspace_application/src/app/modules/profile.module.ts
@@ -2,7 +2,6 @@ import { NgModule , ErrorHandler} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { DatePickerModule } from 'datepicker-ionic2';
 import { ProfilePage } from '../../pages/profile_group/profile/profile';
 import { HelperPage } from '../../pages/profile_group/helper/helper';
 import { SettingPage } from '../../pages/profile_group/setting/setting';
@@ -14,7 +13,7 @@ import { MoverPage } from '../../pages/profile_group/mover/mover';
 
 @NgModule({
   declarations: [ProfilePage, HelperPage, SettingPage, InvitePage, FeedbackPage, RulePage, MoverPage],
-  imports: [IonicModule, CommonModule, DatePickerModule],
+  imports: [IonicModule, CommonModule],
   entryComponents: [ProfilePage, HelperPage, SettingPage, InvitePage, FeedbackPage, RulePage, MoverPage],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
