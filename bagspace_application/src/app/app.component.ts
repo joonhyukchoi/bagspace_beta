9428f18b9fb1664c4a2c54050911e8ec0a506390
CheolWoongChoi <bluesky4381@naver.com>
2017-01-24 14:38:53 +0900

    시작 페이지 수정

--- bagspace_application/src/app/app.component.ts
+++ bagspace_application/src/app/app.component.ts
@@ -2,18 +2,12 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 import { IconPage } from '../pages/tabs_group/tabs/tabs';
-import { ReceiverDatePage } from '../pages/receiver_group/receiver-date/receiver-date';
-import { ReceiverPlacePage } from '../pages/receiver_group/receiver-place/receiver-place';
-import { MoverPage } from '../pages/profile_group/mover/mover';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  //rootPage = IconPage;
-  //rootPage = ReceiverDatePage;
-  //rootPage = MoverPage;
-  rootPage = ReceiverPlacePage;
+  rootPage = IconPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
