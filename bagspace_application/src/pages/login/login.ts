d489b42b731de5f78ae680dc1470bb3e5fb91c2c
YoujunLee <injxyj@gmail.com>
2017-01-29 22:06:32 +0900

    네이버 로그인 api완성

--- bagspace_application/src/pages/login/login.ts
+++ bagspace_application/src/pages/login/login.ts
@@ -1,22 +1,28 @@
+declare var cordova: any
 import { Component } from '@angular/core';
-import { NavController, NavParams, ModalController} from 'ionic-angular';
-import { Facebook, NativeStorage } from 'ionic-native';
+import { NavController, NavParams, ModalController, Platform } from 'ionic-angular';
+import { Facebook, NativeStorage, InAppBrowser} from 'ionic-native';
 import { ProfilePage } from '../profile_group/profile/profile';
- 
+
 /*
   Generated class for the Login page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
+
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
+ 
 export class LoginPage {
+
+
     FB_APP_ID: number = 703053959855280;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform, public modalCtrl: ModalController) {
        Facebook.browserInit(this.FB_APP_ID, "v2.8");}
 
     doFbLogin(){
@@ -58,5 +64,31 @@ export class LoginPage {
     modal.present();
   }
   
-  naver(){ window.open('http://thebagspace.com/login','_blank','location=yet');}
+ naver(){window.open("http://thebagspace.com/login",  "_blank", "location=yes");}
+
+  browser: InAppBrowser;
+  url:string ='http://ionicframework.com/docs/v2/native/inappbrowser/';
+  openBrowser() {
+    let options ='location=no,toolbar=yes,hidden=no';
+    this.browser= new InAppBrowser(this.url,'_blank',options);
+    this.browser.show();
+  }
+  
+  doKtLogin(){
+      if (typeof cordova !== 'undefined') {
+  cordova.plugins.KakaoTalk.login(
+      
+      function (result) {
+        console.log('Successful login!');
+        console.log(result);
+      },
+      function (message) {
+        console.log('Error logging in');
+        console.log(message);
+      }
+  );
+  } 
+  }
 }  
+
+
