9f4abaa0a874745b9fc177e869db7594316a6444
ymg <gt1136@naver.com>
2017-02-03 17:28:12 +0900

    chat db api 완성

--- bagspace_application/src/pages/mover-apply/chat-room/chat-room.ts
+++ bagspace_application/src/pages/mover-apply/chat-room/chat-room.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { Http, Headers } from '@angular/http';
 @Component({
   selector: 'page-chat-room',
   templateUrl: 'chat-room.html',
@@ -10,7 +10,7 @@ export class ChatRoomPage {
   isClick : boolean = false;
 
   //기본 유저 정보
-  myId:string = '123';
+  myId:Number = 123;
   userId:string = '';
 
   //메시지 
@@ -20,29 +20,45 @@ export class ChatRoomPage {
   showMessage: string ='';
 
   //데이터베이스 data
-  data:any= {userId: '123', messageText: 'test1', messageTime: '123'}
-  
+  save_data:any= { id:'',messageText: '', messageTime: ''}
+  data;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http:Http) {
   
+ }
+   ionViewWillEnter() {
+ this.getList();
+}
   send(){
     
     // 메시지 전송 시간 저장
     this.inputMessageTime = new Date();
-    //테스트용
-    this.showMessage = this.inputMessage;
+   
 
-    /*DB로 전송
-    this.data.messageText = this.inputMessage;
-    this.data.messageTime = this.inputMessageTime;
-    
-    ...
-
-    */
-
-    //전송 후 inputMessage 빈 칸 만들어 주기
+    this.save_data.id=123;
+    this.save_data.messageText = this.inputMessage;
+    this.save_data.messageTime = this.inputMessageTime;
     this.inputMessage = '';
-  }
+    var headers = new Headers({'Content-Type': 'application/json'})
+    this.http.post('/mongo_test/chat', this.save_data,{headers: headers})
+    .subscribe(
+      data=> {
+        this.save_data = data.json();
+        this.getList();
+      }
+    )
 
+  }
+getList(){
+  this.http.get('/mongo_test/chat')
+  .subscribe(
+    data=>{
+      this.data = data.json();
+      console.log(data.json());
+    },
+    error =>{
+
+    }
+  );
+}
 }
