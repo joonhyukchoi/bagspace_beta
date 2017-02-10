a92641066ab36040171bead8816dc38e3663216e
YoujunLee <injxyj@gmail.com>
2017-02-10 14:17:47 +0900

    검색필터 적용 시작

--- bagspace_application/src/app/modules/tab.module.ts
+++ bagspace_application/src/app/modules/tab.module.ts
@@ -3,12 +3,13 @@ import { IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { CommonModule } from '@angular/common';
 import { UpperTabsPage } from '../../pages/tabs_group/upper-tabs/upper-tabs';
 import {  IconPage } from '../../pages/tabs_group/tabs/tabs';
+import { SearchPlacePage } from '../../pages/tabs_group/search-place/search-place';
 
 
 @NgModule({
-  declarations: [UpperTabsPage,  IconPage],
+  declarations: [UpperTabsPage,  IconPage, SearchPlacePage],
   imports: [IonicModule, CommonModule],
-  entryComponents: [UpperTabsPage,  IconPage],
+  entryComponents: [UpperTabsPage,  IconPage, SearchPlacePage],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class TabModule {}
---
+++ bagspace_application/src/pages/tabs_group/search-place/search-place.ts
@@ -0,0 +1,211 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-search-place',
+  templateUrl: 'search-place.html'
+})
+export class SearchPlacePage {
+
+ constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public viewCtrl:ViewController) {}
+
+  country_Index : number = -1;
+  city_Index : number = -1;
+  landmark_Index : number = -1;
+
+  selected_Country: string;
+  selected_City: string;
+  selected_Landmark: string;
+  
+  blank = [
+    { name: ' ' },
+    { name: ' ' },
+    { name: ' ' },
+    { name: ' ' }
+  ];
+
+  countries = [
+    { name: '대한민국' },
+    { name: '중국' },
+    { name: '일본' },
+    { name: '이스라엘' }
+  ];
+
+  cities = [
+    { name: ' ' },
+    { name: ' ' },
+    { name: ' ' },
+    { name: ' ' }
+  ];
+
+  landmarks = [
+    { name: ' ' },
+    { name: ' ' },
+    { name: ' ' },
+    { name: ' ' }
+  ];
+
+  korea = [
+    { name: '서울' },
+    { name: '대전' },
+    { name: '부산' },
+    { name: '포항' }
+  ];
+  
+  //korea landmarks
+  seoul = [
+    { name: 'seoul1' },
+    { name: 'seoul2' },
+    { name: 'seoul3' },
+    { name: 'seoul4' }
+  ];
+
+  daejeon = [
+    { name: 'daejeon1' },
+    { name: 'daejeon2' },
+    { name: 'daejeon3' },
+    { name: 'daejeon4' },
+  ]
+
+  pohang = [
+    { name: 'pohang1' },
+    { name: 'pohang2' },
+    { name: 'pohang3' },
+    { name: 'pohang4' },
+  ]
+
+  busan = [
+    { name: 'busan1' },
+    { name: 'busan2' },
+    { name: 'busan3' },
+    { name: 'busan4' },
+  ]
+
+  // ...
+
+  china = [
+    { name: '상하이' },
+    { name: '베이징' },
+    { name: '항저우' },
+    { name: '난징' }
+  ];
+  
+  japan = [
+    { name: '오사카' },
+    { name: '도쿄' },
+    { name: '교토' },
+    { name: '후쿠오카' }
+  ];
+  
+  israel = [
+    { name: '예루살렘' },
+    { name: '텔아이브' },
+    { name: '하이파' },
+    { name: '에일라트' }
+  ];
+
+  //국가 클릭 시
+  countryClick(country, idx, countries){
+    
+    if(this.city_Index != -1){
+      this.cities[this.city_Index]['active'] = false;
+      this.city_Index = -1;
+    }
+    if(this.landmark_Index != -1){
+      this.landmarks[this.landmark_Index]['active'] = false;
+      this.landmark_Index = -1;    
+    }
+    //기존에 다른 국가가 선택되어 있을 시, 마우스 클릭 후 CSS 처리 취소
+    if(this.country_Index != -1)
+      this.countries[this.country_Index]['active'] = false;
+    
+    country['active'] = !country['active'];
+    this.country_Index = idx;
+    this.landmarks = this.blank;
+
+    //date 페이지로 넘김
+    this.selected_Country = country.name;
+
+    switch(country.name){
+      case "대한민국":
+        this.cities = this.korea;
+        break;
+      case "중국":
+        this.cities = this.china;
+        break;
+      case "일본":
+        this.cities = this.japan;
+        break;
+      case "이스라엘":
+        this.cities = this.israel;
+        break;
+    }
+  }
+
+  //도시 클릭 시
+  cityClick(city, idx, cities) {
+
+    if(this.landmark_Index != -1){
+        this.landmarks[this.landmark_Index]['active'] = false;
+        this.landmark_Index = -1;
+    }
+    if(this.city_Index != -1)
+      this.cities[this.city_Index]['active'] = false;
+    
+    city['active'] = !city['active'];
+    this.city_Index = idx;
+
+    //date 페이지로 넘김
+    this.selected_City = city.name;
+
+    switch(city.name){
+      case "서울":
+        this.landmarks = this.seoul;
+        break;
+      case "대전":
+        this.landmarks = this.daejeon;
+        break;
+      case "부산":
+        this.landmarks = this.busan;
+        break;
+      case "포항":
+        this.landmarks = this.pohang;
+        break;
+    }
+
+  }
+
+  landmarkClick(landmark, idx, landmarks){
+
+    if(this.landmark_Index != -1)
+      this.landmarks[this.landmark_Index]['active'] = false;
+    
+    landmark['active']= !landmark['active'];
+    this.landmark_Index = idx;
+
+    //date 페이지로 넘김
+    this.selected_Landmark = landmark.name;
+  }
+
+  cityAlert() {
+    let alert = this.alertCtrl.create({
+      title: '',
+      subTitle: '도시를 선택하세요',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  landmarkAlert() {
+    let alert = this.alertCtrl.create({
+      title: '',
+      subTitle: '랜드마크를 선택하세요',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+}
